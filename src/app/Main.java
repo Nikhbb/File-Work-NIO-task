bd95497e50ca58622d384124c965494e6f0be0a9
vnovikov <nik.hbb@gmail.com>
2024-02-24 04:36:50 +0200

    Add main point

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,42 @@
 package app;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введіть назву файла: ");
+        Helper.BASE_URL = "files/" + sc.next() + ".txt";
+
+        printMenu();
+        int userNumber = sc.nextInt();
+
+        switch (userNumber) {
+            case 0:
+                System.out.println("See you");
+                break;
+            case 1:
+                System.out.print("Введіть текст: ");
+                new FileWriteService().writeToFile(Helper.BASE_URL, sc.next());
+                break;
+            case 2:
+                System.out.println("\n>> Вміст файлу " + Helper.BASE_URL);
+                new FileReadService().readFromFile(Helper.BASE_URL);
+                break;
+            default:
+                System.out.println("Спробуйте ще раз...");
+        }
     }
+
+    private static void printMenu() {
+        System.out.print("""
+                МЕНЮ
+                0 - вихід
+                1 - створити та записати;
+                2 - читати.
+                Ваш вибір:""");
+    }
+
 }
